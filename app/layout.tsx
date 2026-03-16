043a5411a423090ab3b7a8a5f84e2b505c388bfb
Carlos Masso <cmasso@MacBook-Pro-de-Carlos.local>
2026-03-16 21:26:28 +0100

    finish page

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,11 @@ export default async function RootLayout({
         <link rel="alternate" hrefLang="en" href="https://awayna.com/en" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="icon" type="image/png" href="/favicon/-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
       </head>
       <body className="font-sans antialiased">
         {children}
