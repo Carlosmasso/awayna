b73869f752ba08cb395b1181480eebf15579c1be
Carlos Masso <cmasso@MacBook-Pro-de-Carlos.local>
2026-03-17 22:10:36 +0100

    add vercel/speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { getLocale } from "next-intl/server"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Toaster } from "sonner"
 import "./globals.css"
 
@@ -31,6 +32,7 @@ export default async function RootLayout({
         {children}
         <Toaster richColors position="bottom-right" />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
